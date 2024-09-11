2a8fd05b75681d902c388281fbef7f95fe9bdcc9
durunsong <durunsong@sailvan.com>
2024-09-11 15:22:15 +0800

    fix:配置字段更改

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,9 @@
+// scss 无法找到模块问题
+declare module "*.scss" {
+  const scss: Record<string, string>;
+  export default scss;
+}
+
 // TypeScript 无法找到模块问题
 declare module "*.vue" {
   import { defineComponent } from "vue";
