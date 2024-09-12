769d48114b88091bb59eed28ffcce66de016bbb9
durunsong <durunsong@sailvan.com>
2024-09-12 09:48:33 +0800

    🌻fix:修改一些配置

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,7 @@
+<template>
+  <component :is="isAdmin ? Admin : Editor" />
+</template>
+
 <script lang="ts" setup>
 import { useUserStore } from "@/store/modules/user";
 import Admin from "./components/Admin.vue";
@@ -6,7 +10,3 @@ import Editor from "./components/Editor.vue";
 const userStore = useUserStore();
 const isAdmin = userStore.roles.includes("admin");
 </script>
-
-<template>
-  <component :is="isAdmin ? Admin : Editor" />
-</template>
