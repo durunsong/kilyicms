8a7045450b4ba3fb7ee8e13ae714f2b63b5f42bf
durunsong <durunsong@sailvan.com>
2024-09-13 09:33:04 +0800

    🥮feat:生产环境禁用用户调试hook/环境配置

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
+</template>
+
 <script lang="ts" setup>
 import { useTheme } from "@/hooks/useTheme";
 // 将 Element Plus 的语言设置为中文
 import zhCn from "element-plus/es/locale/lang/zh-cn";
-// import { useDetectDevTools } from "./hooks/useDetectDevTools";
-// 禁止用户在浏览器中调试
-// const VITE_MODE = import.meta.env.VITE_MODE;
-// if (VITE_MODE === "production") {
-//   useDetectDevTools();
-// }
+import { useDetectDevTools } from "@/hooks/useDetectDevTools";
+// 生产环境禁止用户在浏览器中调试
+const VITE_MODE = import.meta.env.VITE_MODE;
+
+if (VITE_MODE === "production") {
+  useDetectDevTools();
+}
 
 const { initTheme } = useTheme();
 
 /** 初始化主题 */
 initTheme();
 </script>
-
-<template>
-  <el-config-provider :locale="zhCn">
-    <router-view />
-  </el-config-provider>
-</template>
