f2242a6e4354f213fa514415b0853aeba280a96a
durunsong <durunsong@sailvan.com>
2024-09-18 20:06:51 +0800

    🏖feat:新增elementPlus组件多语言同步切换hooks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <el-config-provider :locale="zhCn">
+  <el-config-provider :locale="elementLocale">
     <router-view />
   </el-config-provider>
 </template>
 
 <script lang="ts" setup>
 import { useTheme } from "@/hooks/useTheme";
-// 将 Element Plus 的语言设置为中文
-import zhCn from "element-plus/es/locale/lang/zh-cn";
+import { useElementPlusLocale } from "@/hooks/useElementPlusLocale";
 import { useDetectDevTools } from "@/hooks/useDetectDevTools";
-// 生产环境禁止用户在浏览器中调试
+
+/** 生产环境禁止用户在浏览器中调试 */
 const VITE_MODE = import.meta.env.VITE_MODE;
 
 if (VITE_MODE === "production") {
@@ -17,6 +17,9 @@ if (VITE_MODE === "production") {
 }
 
 const { initTheme } = useTheme();
+
+/** 使用Element Plus语言环境hooks */
+const { elementLocale } = useElementPlusLocale();
 
 /** 初始化主题 */
 initTheme();
---
+++ src/hooks/useElementPlusLocale.ts
@@ -0,0 +1,62 @@
+// 将 Element Plus 的语言包引入到项目中 切换组件语言hooks
+import { ref, watchEffect } from "vue";
+import { langList } from "@/utils/langList";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import en from "element-plus/es/locale/lang/en";
+import pl from "element-plus/es/locale/lang/pl";
+import ro from "element-plus/es/locale/lang/ro";
+import es from "element-plus/es/locale/lang/es";
+import de from "element-plus/es/locale/lang/de";
+import ar from "element-plus/es/locale/lang/ar";
+import ko from "element-plus/es/locale/lang/ko";
+import el from "element-plus/es/locale/lang/el";
+import pt from "element-plus/es/locale/lang/pt";
+import ru from "element-plus/es/locale/lang/ru";
+import nl from "element-plus/es/locale/lang/nl";
+import fr from "element-plus/es/locale/lang/fr";
+import it from "element-plus/es/locale/lang/it";
+import sv from "element-plus/es/locale/lang/sv";
+import ja from "element-plus/es/locale/lang/ja";
+
+// 从el_lang字段映射到实际的Element Plus语言导入
+const elLocaleMap: Record<string, any> = {
+  "zh-CN": zhCn,
+  en: en,
+  pl: pl,
+  ro: ro,
+  es: es,
+  de: de,
+  ar: ar,
+  ko: ko,
+  el: el,
+  pt: pt,
+  ru: ru,
+  nl: nl,
+  fr: fr,
+  it: it,
+  sv: sv,
+  ja: ja,
+};
+
+export const useElementPlusLocale = () => {
+  const elementLocale = ref(zhCn); // 默认语言
+
+  const setElementLocale = () => {
+    const localLang = localStorage.getItem("localLang") || "en"; // 默认设置英文
+    const langObj = langList.find((lang) => lang.category === localLang);
+
+    if (langObj && elLocaleMap[langObj.el_lang]) {
+      elementLocale.value = elLocaleMap[langObj.el_lang];
+    } else {
+      elementLocale.value = elLocaleMap["en"]; // Fallback 默认英文
+    }
+  };
+
+  watchEffect(() => {
+    setElementLocale();
+  });
+
+  return {
+    elementLocale,
+  };
+};
