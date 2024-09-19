14cb2389405b1b8abc95cf21cf30b26f4acbc7fd
durunsong <durunsong@sailvan.com>
2024-09-19 10:36:02 +0800

    🎑fix:修改eslint规则

--- eslint.config.js
+++ eslint.config.js
@@ -73,7 +73,7 @@ export default [
           html: {
             void: "never",
             normal: "never",
-            component: "always",
+            component: "never",
           },
           svg: "always",
           math: "always",
