60bceef71764f44013bd5ded5bc878f7baf34c9e
durunsong <durunsong@sailvan.com>
2024-10-17 17:42:15 +0800

    ♨fix：修改样式错误

--- src/layouts/components/NavigationBar/index.vue
+++ src/layouts/components/NavigationBar/index.vue
@@ -86,12 +86,12 @@ const logout = () => {
 </script>
 
 <style lang="scss" scoped>
-.navigation-tionbar-height {
+.navigation-bar {
+  height: var(--kilyicms-navigationbar-height);
   overflow: hidden;
   color: var(--kilyicms-navigationbar-text-color);
   display: flex;
   justify-content: space-between;
-
   .hamburger {
     display: flex;
     align-items: center;
@@ -99,25 +99,20 @@ const logout = () => {
     padding: 0 15px;
     cursor: pointer;
   }
-
   .breadcrumb {
     flex: 1;
-
     // 参考 Bootstrap 的响应式设计将宽度设置为 576
     @media screen and (max-width: 576px) {
       display: none;
     }
   }
-
   .sidebar {
     flex: 1;
     // 设置 min-width 是为了让 Sidebar 里的 el-menu 宽度自适应
     min-width: 0px;
-
     :deep(.el-menu) {
       background-color: transparent;
     }
-
     :deep(.el-sub-menu) {
       &.is-active {
         .el-sub-menu__title {
@@ -126,25 +121,20 @@ const logout = () => {
       }
     }
   }
-
   .right-menu {
     margin-right: 10px;
     height: 100%;
     display: flex;
     align-items: center;
-
     .right-menu-item {
       padding: 0 10px;
       cursor: pointer;
-
       .right-menu-avatar {
         display: flex;
         align-items: center;
-
         .el-avatar {
           margin-right: 10px;
         }
-
         span {
           font-size: 16px;
         }
