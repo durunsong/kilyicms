f73e1fd2bb5595558b1c6b2e345ccd4a72c85914
durunsong <durunsong@sailvan.com>
2024-11-20 20:39:09 +0800

    🌭feat:添加生产包

--- static/index-DC72Hr0J.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index-DLWXmvD5.js";import{ai as t,p as r,q as n,V as c}from"./vue-utQA5pq8.js";import"./element-DFbVGgsZ.js";const s={},a={class:"app-container"};function _(i,p){const e=t("router-view");return r(),n("div",a,[c(e)])}const l=o(s,[["render",_]]);export{l as default};
