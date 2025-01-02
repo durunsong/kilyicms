f73f34d53719a48089b327c1cc0945e9f581284e
durunsong <durunsong@sailvan.com>
2025-01-02 11:08:20 +0800

    🥭fix：404/403页面更改

--- src/views/error-page/403.vue
+++ src/views/error-page/403.vue
@@ -1,10 +1,10 @@
 <template>
   <ErrorPageLayout>
-    <Svg403 />
+    <Svg403></Svg403>
   </ErrorPageLayout>
 </template>
 
 <script lang="ts" setup>
 import ErrorPageLayout from "./components/ErrorPageLayout.vue";
-import Svg403 from "@/assets/error-page/403.svg?component"; // vite-svg-loader 插件的功能
+import Svg403 from "@/assets/error-page/403.svg"; // vite-svg-loader 插件的功能
 </script>
--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -1,10 +1,10 @@
 <template>
   <ErrorPageLayout>
-    <Svg404 />
+    <Svg404></Svg404>
   </ErrorPageLayout>
 </template>
 
 <script lang="ts" setup>
 import ErrorPageLayout from "./components/ErrorPageLayout.vue";
-import Svg404 from "@/assets/error-page/404.svg?component"; // vite-svg-loader 插件的功能
+import Svg404 from "@/assets/error-page/404.svg"; // vite-svg-loader 插件的功能
 </script>
