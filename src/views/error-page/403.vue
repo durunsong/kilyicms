23d55e2a87791beb9cf1146db503c15d48d75b69
durunsong <durunsong@sailvan.com>
2025-01-06 15:57:32 +0800

    🍆fix：伪造URL--redirect错误问题

--- src/views/error-page/403.vue
+++ src/views/error-page/403.vue
@@ -6,5 +6,6 @@
 
 <script lang="ts" setup>
 import ErrorPageLayout from "./components/ErrorPageLayout.vue";
-import Svg403 from "@/assets/error-page/403.svg"; // vite-svg-loader 插件的功能
+// @ts-ignore
+import Svg403 from "@/assets/error-page/403.svg?component"; // vite-svg-loader 插件的功能
 </script>
--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -6,5 +6,6 @@
 
 <script lang="ts" setup>
 import ErrorPageLayout from "./components/ErrorPageLayout.vue";
-import Svg404 from "@/assets/error-page/404.svg"; // vite-svg-loader 插件的功能
+// @ts-ignore
+import Svg404 from "@/assets/error-page/404.svg?component"; // vite-svg-loader 插件的功能
 </script>
